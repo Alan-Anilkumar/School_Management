929f33dad0f72a4e3564c30c010bcb73c26b60de
YourUsername <your-email@example.com>
2024-10-24 17:09:54 +0530

    complete

--- school_management/settings/base.py
+++ school_management/settings/base.py
@@ -1,19 +1,17 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+load_dotenv()
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6jy@q6mn4$!8ydy@yng-j@vtuktcjagq9d+trwyu&_sa3dnc&p"
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG", default=False)
 ALLOWED_HOSTS = []
 
 
